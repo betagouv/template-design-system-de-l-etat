fa04f2e1af374ebb5df191f8272eb17f51be6e62
Estelle Comment <estelle.comment@gmail.com>
2020-11-18 11:41:00 +0100

    Basic design system

--- index.js
+++ index.js
@@ -2,16 +2,23 @@ const express = require('express')
 const path = require('path')
 
 const appName = 'Renforts'
-const port = 8080 // todo scalingo needs this in vars or something ?
+const appDescription = 'Mise à disposition d\'agents pour aider au contact tracing Covid'
+const appRepo = 'https://github.com/betagouv/renforts'
+const port = 8080
 
 const app = express()
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+app.use('/static', express.static('static'))
+// Hack for importing css from npm package
+app.use('/~', express.static(path.join(__dirname, 'node_modules')))
 // Populate some variables for all views
 app.use(function(req, res, next){
   res.locals.appName = appName
+  res.locals.appDescription = appDescription
+  res.locals.appRepo = appRepo
   next()
 })
 
